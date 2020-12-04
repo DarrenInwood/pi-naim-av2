70a9f370d3e600447a9b324e245a8e923a542350
Darren Inwood <darren.inwood@gmail.com>
2020-12-04 14:16:12 +1300

    Automatically add mock tty for dev server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { NaimAV2 } from "./naim-av2/naim-av2";
+import { debug } from 'debug';
 import SerialPort from "serialport";
+import MockBinding from "@serialport/binding-mock";
+
+const log = debug('pi-naim-av2:main');
 
 // // For testing without a serial port
-// import MockBinding from "@serialport/binding-mock";
-// SerialPort.Binding = MockBinding;
-// MockBinding.createPort('/dev/ttyUSB0', { echo: true, record: true })
+if (log.enabled) {
+    SerialPort.Binding = MockBinding;
+    MockBinding.createPort('/dev/ttyUSB0', { echo: true, record: true })
+}
 
 const av2 = new NaimAV2({
     comPort: '/dev/ttyUSB0'
