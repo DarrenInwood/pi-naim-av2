48cc20e0a4155fed6dbf255dc3ab8c22ae252a4b
DarrenInwood <darren.inwood@gmail.com>
2020-12-18 14:53:03 +1300

    Update HdmiCecAudioSystemDevice.ts

--- src/hdmi-cec-audio-system-device/HdmiCecAudioSystemDevice.ts
+++ src/hdmi-cec-audio-system-device/HdmiCecAudioSystemDevice.ts
@@ -1,8 +1,9 @@
 
 
-import { EventEmitter } from "events";
-import { CEC, CECMonitor} from '@senzil/cec-monitor';
 import { debug } from 'debug';
+import { EventEmitter } from 'events';
+
+import { CEC, CECMonitor } from '@senzil/cec-monitor';
 
 /**
  * Constructor options
@@ -31,34 +32,37 @@ const log = debug('pi-naim-av2:HdmiCecAudioSystemDevice');
 /**
  * Provides the behaviour of an HDMI-CEC "Audio System" type device.
  * 
- * Mandatory messages the device must send/receive:
- * Poll - send/receive (A device may poll logical addresses no more than once every 30 seconds)
- * 0x00 Feature abort - send/receive
- * 0x04 Image view on - send (advertises to the TV that this source has something to display)
- * 0x0D Text view on - send
- * 0x36 Standby - receive (can send)
- * 0x44 User control pressed - receive
- * 0x45 User control released - receive
- * 0x46 Give OSD name - receive
- * 0x70 System audio mode request - receive
- * 0x71 Give audio status - receive
- * 0x72 Set System audio mode - receive
- * 0x7A Report audio status - send
- * 0x7D Give system audio mode status - receive
- * 0x7E System audio mode status - send
- * 0x82 Active source - send/receive
- * 0x83 Give physical address - receive
- * 0x84 Report physical address - send
- * 0x85 Request active source - receive
- * 0x87 Device vendor ID - receive
- * 0x8C Give device vendor ID - send
- * 0x8F Give device power status - receive
- * 0x90 Report power status - send
- * 0x9D Inactive source - send
- * 0x9E Report CEC version - send (05:9E:03 = CEC 1.4)
- * 0x9F Get CEC version - receive
- * 0xA3 Report short audio descriptor - send
- * 0xA4 Request short audio descriptor - receive
+ * Mandatory messages the device must send/receive: (X = mandatory, O = optional)
+ * 
+ * Hex  Name                            Implemented  Send Receive Comments
+ * Poll                                 (recv)       X    X       No more than once every 30 seconds
+ * 0x00 Feature abort                                X    X       Default response to directly addressed messages we don't respond to
+ * 0x04 Image view on                   -            X            Advertises to the TV that this source has something to display
+ * 0x0D Text view on                    -            X
+ * 0x36 Standby                         X            O    X       If all sources are in standby mode, AV2 should power off
+ * 0x44 User control pressed                              X       See table below for mandatory/optional keys to respond to
+ * 0x45 User control released                             X       See table below for mandatory/optional keys to respond to
+ * 0x46 Give OSD name                   X                 X       Responds: 0x47 Report OSD name
+ * 0x47 Report OSD name                 X            O            
+ * 0x70 System audio mode request       X                 X
+ * 0x71 Give audio status               X                 X       Responds: 0x7A Report audio status
+ * 0x72 Set System audio mode           X                 X       Can be addressed of broadcast. Indicates to AV2 whether the TV wants it to be on or off
+ * 0x7A Report audio status             X            X
+ * 0x7D Give system audio mode status                     X       Responds: 0x7E System audio mode status
+ * 0x7E System audio mode status                     X
+ * 0x82 Active source                                X    X       Received from TV, should set AV2 input selector
+ * 0x83 Give physical address                             X       Responds: 0x84 Report physical address
+ * 0x84 Report physical address                      X
+ * 0x85 Request active source                             X
+ * 0x87 Device vendor ID                X            X
+ * 0x8C Give device vendor ID           X                 X       Responds: 0x87 Device vendor ID
+ * 0x8F Give device power status        X                 X       Responds: 0x90 Report power status
+ * 0x90 Report power status             X            X
+ * 0x9D Inactive source                              X
+ * 0x9E Report CEC version              X            X            (05:9E:03 = CEC 1.4)
+ * 0x9F Get CEC version                 X                 X       Responds: 0x9E Report CEC version
+ * 0xA3 Report short audio descriptor                X
+ * 0xA4 Request short audio descriptor                    X
  * 
  * Mandatory remote control keypresses the device should forward to the TV:
  * 0x00 Select
@@ -69,14 +73,29 @@ const log = debug('pi-naim-av2:HdmiCecAudioSystemDevice');
  * 0x41 Volume up
  * 0x42 Volume down
  * 0x65 Mute
- * 0x66 Unmute (optional)
- * 0x69 Select AV Input function (optional)
- * 0x6A Select Audio input (optional)
- * 0x6B Power toggle (optional)
- * 0x6C Power off (optional)
- * 0x6D Power on (optional)
- * 2
+ * 
+ * Optional keypresses the device should forward to the TV:
+ * 0x66 Unmute
+ * 0x69 Select AV Input function
+ * 0x6A Select Audio input
+ * 0x6B Power toggle
+ * 0x6C Power off
+ * 0x6D Power on
  */
+
+interface CecPacket {
+    type: string,
+    number: string,
+    flow: 'IN' | 'OUT',
+    source: number, // int
+    target: number, // int
+    opcode: number, // int
+    args: number[], // int
+    data?: {
+        val: string,
+        str: string
+    }
+}
 
 export class HdmiCecAudioSystemDevice extends EventEmitter {
 
@@ -97,7 +116,7 @@ export class HdmiCecAudioSystemDevice extends EventEmitter {
         power: false,
         mute: false,
         volume: 0,
-        physicalAddress: [0,0,0,0],
+        physicalAddress: [0x00, 0x00, 0x05],
         activeSource: false
     };
 
@@ -140,12 +159,12 @@ export class HdmiCecAudioSystemDevice extends EventEmitter {
             this.options.osdName
         ); 
 
-        // TODO: Send polling messages every 45sec
-        // this.pollingInterval = setInterval(() = {
+        // TODO: Send polling messages every 45sec?
+        // this.pollingInterval = setInterval(() => {
         //     this.monitor.WriteMessage(
         //         CEC.LogicalAddress.AUDIOSYSTEM,
         //         CEC.LogicalAddress.BROADCAST,
-        //         CEC.Opcode.???
+        //         CEC.Opcode.
         //     );
         // }, 45000);
     }
@@ -154,7 +173,7 @@ export class HdmiCecAudioSystemDevice extends EventEmitter {
      * Listen for the various messages we should respond to
      */
     protected setupListeners() {
-        this.monitor.on(CECMonitor.EVENTS.GET_CEC_VERSION, (event: any) => {
+        this.monitor.on(CECMonitor.EVENTS.GET_CEC_VERSION, (event: CecPacket) => {
             log('<<< GET_CEC_VERSION - sending VERSION_1_4');
             this.monitor.SendMessage(
                 CEC.LogicalAddress.AUDIOSYSTEM,
@@ -165,11 +184,8 @@ export class HdmiCecAudioSystemDevice extends EventEmitter {
         });
 
         // TODO: how to respond to polling messages?
-        this.monitor.on(CECMonitor.EVENTS.POLLING_MESSAGE, (event: any) => {
-            log('<<< POLLING_MESSAGE - sending ???');
-            // this.monitor.SendMessage(
-
-            // );
+        this.monitor.on(CECMonitor.EVENTS.POLLING_MESSAGE, (event: CecPacket) => {
+            log('<<< POLLING_MESSAGE');
         });
 
         this.monitor.on(CECMonitor.EVENTS.STANDBY, () => {
@@ -183,8 +199,28 @@ export class HdmiCecAudioSystemDevice extends EventEmitter {
             this.emit('stateChange', this.state, prevState);            
         });
 
-        this.monitor.on(CECMonitor.EVENTS.GIVE_OSD_NAME, (event: any) => {
-            log('<<< GIVE_OSD_NAME - sending %s', this.options.osdName);
+        this.monitor.on(CECMonitor.EVENTS.GIVE_DEVICE_VENDOR_ID, (event: CecPacket) => {
+            log('<<< GIVE_DEVICE_VENDOR_ID - sending DEVICE_VENDOR_ID UNKNOWN');
+            this.monitor.SendMessage(
+                CEC.LogicalAddress.AUDIOSYSTEM,
+                CEC.LogicalAddress.BROADCAST,
+                CEC.Opcode.DEVICE_VENDOR_ID,
+                CEC.VendorId.UNKNOWN
+            );
+        });
+        
+        this.monitor.on(CECMonitor.EVENTS.GIVE_DEVICE_POWER_STATUS, (event: CecPacket) => {
+            log('<<< GIVE_DEVICE_POWER_STATUS - sending REPORT_POWER_STATUS %s', (this.state.power ? 'ON' : 'STANDBY'));
+            this.monitor.SendMessage(
+                CEC.LogicalAddress.AUDIOSYSTEM,
+                event.source,
+                CEC.Opcode.REPORT_POWER_STATUS,
+                (this.state.power ? CECMonitor.PowerStatus.ON : CECMonitor.PowerStatus.STANDBY)
+            );
+        });
+
+        this.monitor.on(CECMonitor.EVENTS.GIVE_OSD_NAME, (event: CecPacket) => {
+            log('<<< GIVE_OSD_NAME - sending SET_OSD_NAME %s', this.options.osdName);
             this.monitor.SendMessage(
                 CEC.LogicalAddress.AUDIOSYSTEM,
                 event.source,
@@ -193,7 +229,41 @@ export class HdmiCecAudioSystemDevice extends EventEmitter {
             );
         });
 
-        
+        this.monitor.on(CECMonitor.EVENTS.GIVE_PHYSICAL_ADDRESS, (event: CecPacket) => {
+            log('<<< GIVE_PHYSICAL_ADDRESS - sending REPORT_PHYSICAL_ADDRESS %s', this.state.physicalAddress.toString());
+            this.monitor.SendMessage(
+                CEC.LogicalAddress.AUDIOSYSTEM,
+                CEC.LogicalAddress.BROADCAST,
+                CEC.Opcode.REPORT_PHYSICAL_ADDRESS,
+                this.state.physicalAddress // TODO: is this correct? 
+            );
+        });
+
+        this.monitor.on(CECMonitor.EVENTS.SYSTEM_AUDIO_MODE_REQUEST, (event: CecPacket) => {
+            log('<<< SYSTEM_AUDIO_MODE_REQUEST - sending SET_SYSTEM_AUDIO_MODE %s', (this.state.power ? 'ON' : 'OFF'));
+            this.monitor.SendMessage(
+                CEC.LogicalAddress.AUDIOSYSTEM,
+                CEC.LogicalAddress.BROADCAST,
+                CEC.Opcode.SET_SYSTEM_AUDIO_MODE,
+                this.state.power ? CEC.SystemAudioStatus.ON : CEC.SystemAudioStatus.OFF
+            );
+        });
+
+        this.monitor.on(CECMonitor.EVENTS.GIVE_AUDIO_STATUS, (event: CecPacket) => {
+            log(
+                '<<< GIVE_AUDIO_STATUS - sending REPORT_AUDIO_STATUS %s/%s',
+                this.state.volume,
+                (this.state.power ? 'ON' : 'OFF')
+            );
+            this.monitor.SendMessage(
+                CEC.LogicalAddress.AUDIOSYSTEM,
+                CEC.LogicalAddress.BROADCAST,
+                CEC.Opcode.REPORT_AUDIO_STATUS,
+                this.state.volume | (this.state.power ? 0x80 : 0x00)
+            );
+        });
+
+
     }
 
 }
